67dcd18af199e228983c4af3788889a3cca7b95b
luisgch2004 <luis.guevarac@unmsm.edu.pe>
2024-11-05 01:25:40 -0500

    Funcionalidades de ingresos

--- BRENIS/trabajo_procesos/src/main/java/com/datos/Ingresos.java
+++ BRENIS/trabajo_procesos/src/main/java/com/datos/Ingresos.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Ingresos {
     
     private List<Ingreso> ingresos;
-    private int contador_id = 1;
+    private int contador_id = 0;
 
     public Ingresos() {
         ingresos = new ArrayList<>();
